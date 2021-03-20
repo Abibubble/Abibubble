ea54d2a6f27e9a1c20ab5c403e84af8187fbf9c1
Abibubble <abi.yamaha.neo@gmail.com>
2021-03-20 22:33:56 +0000

    feat: Add basic slideshow function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// --------------------------------------------------------------------- Copyright
+
 function copyrightYear() {
     var d = new Date();
     var y = d.getFullYear();
@@ -5,3 +7,28 @@ function copyrightYear() {
 }
 
 copyrightYear();
+
+// --------------------------------------------------------------------- Slideshow
+
+var slideshow = document.getElementById("slideshow");
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    slideshow.classList.remove("header-image1");
+    slideshow.classList.remove("header-image2");
+    slideshow.classList.remove("header-image3");
+    if (slideIndex === 0) {
+        slideshow.classList.add("header-image1");
+        slideIndex++;
+    } else if (slideIndex === 1) {
+        slideshow.classList.add("header-image2");
+        slideIndex++;
+    } else if (slideIndex === 2) {
+        slideshow.classList.add("header-image3");
+        slideIndex = 0;
+    } else {
+        console.log("Error with slideshow images");
+    }
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
